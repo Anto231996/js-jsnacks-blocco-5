0e48ffd467875668996b91ef62283d10f11b511a
Anto231996 <tony2396@hotmail.it>
2022-03-11 17:15:16 +0100

    commit

--- jsnack E/script.js
+++ jsnack E/script.js
@@ -1,47 +1,68 @@
 /**
- * 
- * 
-    Crea un array di oggetti che rappresentano degli animali.
-    Ogni animale ha un nome, una famiglia e una classe.
-    Es:
-
-    [
-        { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
-        { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
-        { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
-    ]
-
-    Crea un nuovo array con la lista dei mammiferi.
-
- * 
+ *
+    Crea un array di oggetti che rappresentino delle persone.
+    Ogni persona ha un nome, un cognome e un'età.
+    
+    Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
  */
+const puoiGuidare = []
+const nonPuoiGuidare = []
+const people = [
+   {
+      firstName: 'Wayne',
+      lastName: 'Barnett',
+      age: '21',
+   },
+
+   {
+      firstName: 'Angela',
+      lastName: 'Caroll',
+      age: '17',
+   },
+
+   {
+      firstName: 'Walter',
+      lastName: 'Gordon',
+      age: '127',
+   },
+
+   {
+      firstName: 'Angela',
+      lastName: 'Lopez',
+      age: '16',
+   },
+
+   {
+      firstName: 'Scott',
+      lastName: 'Estrada',
+      age: '44',
+   },
+
+   {
+      firstName: 'Barbara',
+      lastName: 'Ramos',
+      age: '89',
+   }
+];
 
 
-const animali =
-[
-  {
-    nome: 'leone', 
-    famiglia: 'felidi', 
-    classe: 'mammiferi'
-  },
+const maggiorenne = people.filter((element) =>{
+   if (element.age >= 18 ) return true;
 
-  {
-    nome: 'cane', 
-    famiglia: 'canisi', 
-    classe: 'mammiferi'
-  },
-
-  {
-    nome: 'gallina', 
-    famiglia: 'fasianidi', 
-    classe: 'uccelli'
-  }
-]
-
-const mammiferi = animali.filter((element) =>{
-  if (element.classe == "mammiferi") return true;
 })
 
+puoiGuidare.push(maggiorenne);
 
-console.table(animali)
-console.table(mammiferi)
+const minorenne = people.filter((element) =>{
+   if (element.age < 18 ) return true;
+})
+
+nonPuoiGuidare.push(minorenne);
+
+
+
+console.table(maggiorenne);
+console.table(minorenne);
+
+console.table(puoiGuidare)
+console.table(nonPuoiGuidare)
